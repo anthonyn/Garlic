fdc86606bb557cad7abae6ea78454ff26f0c223e
anthonyn <anthony.nicolaides@gmail.com>
2019-08-13 22:17:12 -0400

    added rainbow function

--- colorblast/colorblast.py
+++ colorblast/colorblast.py
@@ -38,13 +38,42 @@ else:
     print 'WARNING: Client3 could not connect to %s' % ADDRESS3
 
 
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    # if pos < 0 or pos > 255:
+    #     r = g = b = 0
+    if pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos*3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos*3)
+        g = 0
+        b = int(pos*3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos*3)
+        b = int(255 - pos*3)
+    return (r, g, b) 
+
+
+#Leave this its the index value for the rainbow
+i = 0
+
+
+#make this bigger and smaller to control transistion speed
+time = 1
 
 # Send pixels forever
 while True:
     #color = (color + 1 ) % 255
     
-    color = 255
-    my_pixels = [(color, color, color)] * 255
+    i = (i + 1 ) % 255 
+    color = wheel(i)
+    my_pixels = [color] * 255
 
     for client in clients:
     	#random.shuffle(my_pixels)
@@ -63,5 +92,5 @@ while True:
 
 
     #tune the sleep here          
-    time.sleep(0.1)
+    time.sleep(1)
 
